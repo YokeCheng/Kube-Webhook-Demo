8a58c2c276d4261e8d302bf91473150c5e9ff919
yoke <18322535679@163.com>
2022-06-24 14:57:35 +0800

    [devops]1.初始化项目 kubebuilder init --domain=kruise.io --license none --owner "Yoke" 2.创建api kubebuilder create api --group core --version v1 --kind Pod --resource=false --controller=false 3.创建webhook kubebuilder create webhook --group core --version v1 --kind Pod --programmatic-validation

---
+++ api/v1/pod_webhook.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2022 Yoke.
+*/
+
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var podlog = logf.Log.WithName("pod-resource")
+
+func (r *Pod) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-core-v1-pod,mutating=false,failurePolicy=fail,sideEffects=None,groups=core,resources=pods,verbs=create;update,versions=v1,name=vpod.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &Pod{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *Pod) ValidateCreate() error {
+	podlog.Info("validate create", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object creation.
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *Pod) ValidateUpdate(old runtime.Object) error {
+	podlog.Info("validate update", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object update.
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *Pod) ValidateDelete() error {
+	podlog.Info("validate delete", "name", r.Name)
+
+	// TODO(user): fill in your validation logic upon object deletion.
+	return nil
+}
